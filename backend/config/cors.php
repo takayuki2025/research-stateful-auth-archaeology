a9ac44cef87ff6c89960c7eb0cd5759c17038dbd
kawada takayuki <t.principle.k2024@gmail.com>
2026-01-22 22:38:20 +0900

    feat: implement new functionality03-28

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,9 +6,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
-    ],
+    'allowed_origins' => array_values(array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost'))))),
+
+    // 'allowed_origins' => [
+    //     env('FRONTEND_URL', 'http://localhost:3000'),
+    // ],
 
     'allowed_origins_patterns' => [],
 
